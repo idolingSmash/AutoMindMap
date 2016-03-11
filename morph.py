fe4543929a9d030d478c0d33b56ce7cdd812c918
Kazuo Sato <road2smileforall@gmail.com>
2016-03-11 20:06:43 +0900

    First Commit

---
+++ morph.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import urllib.request, urllib.error
+from bs4 import BeautifulSoup
+
+wikiURL = "https://ja.wikipedia.org/wiki/%E3%83%A1%E3%82%A4%E3%83%B3%E3%83%9A%E3%83%BC%E3%82%B8"
+
+def main():
+    print(wikiURL)
+
+
+if __name__ == '__main__':
+    main()
